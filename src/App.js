d9896547dabec18389e955d363d9e6b3ce94f705
Obakeng Freddy Diphoko <42438019+diphokoo@users.noreply.github.com>
2024-03-01 09:29:58 +0200

    creating dropdown file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import SearchInput from './SearchInPut';
+import DrpField from './drpDown';
 
 function App() {
   return (
     <div className="App">
       <h2>search Input</h2>
       <SearchInput />
+      <DrpField />
     </div>
   );
 }
---
+++ src/drpDown.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DrpField() {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+export default DrpField;
